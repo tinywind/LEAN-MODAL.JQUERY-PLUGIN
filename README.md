92cb7b5d54d9cba381d6f731083bd49e1cbc6d52
Jeon JaeHyeong <tinywind@linkbiz.kr>
2016-06-03 10:06:42 +0900

    initialize

---
+++ README.md
@@ -0,0 +1,15 @@
+# jquery.leanModal.js
+Origin: https://github.com/FinelySliced/leanModal.js __(http://leanmodal.finelysliced.com.au/)__
+ A super simple JQuery plugin for modal windows. works with your CSS. 
+ Built for all the short dialogs, alerts, panels and such associated with an app, that you may want to handle in a modal window. Designed to handle hidden content, and doesn't apply any styles to the target element, other than for displaying and positioning.
+ 
+# Propose
+* support stacked popup
+
+# How to Use
+1. {jQuery Object}.leanModal();
+1. Mandatory condition: {DOM Object} must have a attribute: 'href' or 'data-href'. When this object({DOM Object}) is clicked, $({jQuery Object}.attr(\[{href}|{data-href}\])) is show.
+1. Options: This Js is supported only 'closeButton' option.
+
+# Licensing
+Available under the MIT and GPL licenses.
--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -1,73 +1,119 @@
-(function($){
- 
-    $.fn.extend({ 
-         
-        leanModal: function(options) {
- 
-            var defaults = {
-                top: 100,
-                overlay: 0.5,
-                closeButton: null
+// leanModal v1.1 by Ray Stone - http://finelysliced.com.au
+// Dual licensed under the MIT and GPL
+
+// Modify by tinywind
+
+(function ($) {
+    window.leanModal = {
+        _stackModal: [],
+        _stackZIndex: [],
+        _zIndexBase: 10000,
+        _zIndexInterval: 10,
+        getModalZIndex: function (modal) {
+            for (var i = 0; i < this._stackModal.length; i++)
+                if (this._stackModal[i] == modal) {
+                    console.error("already contains modal");
+                    return null;
+                }
+            this._stackModal.push(modal);
+
+            if (this._stackZIndex.length == 0) {
+                var zIndex = this._zIndexBase;
+                this._stackZIndex.push(zIndex);
+                return zIndex;
+            } else {
+                zIndex = this._stackZIndex[this._stackZIndex.length - 1] + this._zIndexInterval;
+                this._stackZIndex.push(zIndex);
+                return zIndex;
             }
-            
-            var overlay = $("<div id='lean_overlay'></div>");
-            
-            $("body").append(overlay);
-                 
-            options =  $.extend(defaults, options);
- 
-            return this.each(function() {
-            
-                var o = options;
-               
-                $(this).click(function(e) {
-              
-              	var modal_id = $(this).attr("href");
+        },
+        checkHideLeanOverlayContainer: function (modal) {
+            for (var i = 0; i < this._stackModal.length; i++)
+                if (this._stackModal[i] == modal) {
+                    this._stackModal.splice(i, 1);
+                    this._stackZIndex.splice(i, 1);
+                }
 
-				$("#lean_overlay").click(function() { 
-                     close_modal(modal_id);                    
-                });
-                
-                $(o.closeButton).click(function() { 
-                     close_modal(modal_id);                    
-                });
-                         	
-              	var modal_height = $(modal_id).outerHeight();
-        	  	var modal_width = $(modal_id).outerWidth();
-
-        		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
-
-        		$('#lean_overlay').fadeTo(200,o.overlay);
-
-        		$(modal_id).css({ 
-        		
-        			'display' : 'block',
-        			'position' : 'fixed',
-        			'opacity' : 0,
-        			'z-index': 11000,
-        			'left' : 50 + '%',
-        			'margin-left' : -(modal_width/2) + "px",
-        			'top' : o.top + "px"
-        		
-        		});
-
-        		$(modal_id).fadeTo(200,1);
-
-                e.preventDefault();
-                		
-              	});
-             
-            });
-
-			function close_modal(modal_id){
-
-        		$("#lean_overlay").fadeOut(200);
-
-        		$(modal_id).css({ 'display' : 'none' });
-			
-			}
-    
+            return this._stackModal.length == 0;
         }
-    });
-     
+    };
+
+    $.fn.extend({
+        leanModal: function (options) {
+            var defaults = {closeButton: null};
+            options = $.extend(defaults, options);
+            var overlayContainer = $('#lean-overlay-container');
+            if (overlayContainer.length == 0)
+                overlayContainer = $('<div/>', {
+                    id: 'lean-overlay-container'
+                }).bind('mousewheel keydown keyup', function (e) {
+                    //e.preventDefault();
+                    e.stopPropagation();
+                }).css({
+                    position: 'fixed',
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    display: 'none'
+                }).appendTo($('body'));
+
+            return this.each(function () {
+                var overlay = $('<div/>', {
+                    'class': 'lean-overlay'
+                }).css({
+                    display: "none",
+                    opacity: 0.5,
+                    position: "absolute",
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    'background-color': 'black'
+                });
+                overlayContainer.append(overlay);
+
+                function showModal(modal, overlay) {
+                    overlayContainer.show();
+                    var zIndex = window.leanModal.getModalZIndex(modal[0]);
+                    var top = (window.innerHeight - modal.outerHeight()) / 2;
+                    modal.css({
+                        'z-index': zIndex,
+                        top: top < 0 ? "0px" : top + "px"
+                    }).show()
+                        .focus();
+                    overlay.css({'z-index': zIndex - 1}).show();
+                }
+
+                function closeModal(modal, overlay) {
+                    modal.hide();
+                    overlay.hide();
+                    if (overlayContainer.find('.lean-overlay').filter(function () {
+                            if (this == overlay[0]) return false;
+                            return $(this).is(':hidden');
+                        }) == 0)
+                        overlayContainer.hide();
+                    if (window.leanModal.checkHideLeanOverlayContainer(modal[0]))
+                        overlayContainer.hide();
+                }
+
+                var href = $(this).attr("href");
+                var modal = $(href != null ? href : $(this).attr('data-href'));
+                modal.css({
+                    display: "none",
+                    position: "absolute",
+                    left: 50 + "%",
+                    'margin-left': -(modal.outerWidth() / 2) + "px"
+                }).detach().appendTo(overlayContainer);
+
+                $(this).click(function () {
+                    showModal(modal, overlay);
+                });
+
+                $(options.closeButton).click(function () {
+                    closeModal(modal, overlay);
+                });
+            });
+        }
+    })
 })(jQuery);
